62fe6ae6e59d05815ad0f68a21b0ecb8336ea531
Ntshembo <juniorWebProjects@gmail.com>
2020-12-06 20:45:18 +0200

    Bug Fix: Controller export breaking
    When new instance is created failure to access the methods, hence change from classes to Objects to test if possible to access this methods in this format

--- src/Auth.js
+++ src/Auth.js
@@ -2,168 +2,164 @@ import nodemailer from "nodemailer";
 import Formidable from "formidable";
 import Bcrypt from "bcrypt";
 
-class ExpressSessionAuth {
-  constructor(
-    userModel,
-    userSessions,
-    mailTransporterUser,
-    mailTransporterPass,
-    passwordLength,
-    hashRounds
-  ) {
-    this.userModel = userModel;
-    this.mailTransporterUser = mailTransporterUser;
-    this.mailTransporterPass = mailTransporterPass;
-    this.passwordLength = passwordLength || 8;
-    this.hashRounds = hashRounds || 15;
-    this.userSessions = userSessions;
-  }
+// userModel,
+// userSessions,
+// mailTransporterUser,
+// mailTransporterPass,
+// passwordLength,
+// hashRounds
 
-  SignUp(request, response) {
-    const form = new Formidable.IncomingForm();
+const Auth = {
+  passwordLength: 8,
+  userModel: null,
+  userSessions: null,
+  mailTransporterUser: null,
+  mailTransporterPass: null,
+  hashRounds: 15,
+};
 
-    try {
-      form.parse(request, async (error, fields, files) => {
-        if (error) {
-          return response
-            .status(500)
-            .json({ msg: "Network Error: Failed to signup user" });
-        }
+Auth.SignUp = (request, response) => {
+  const form = new Formidable.IncomingForm();
 
-        const { username, email, password, verifiedPassword } = fields;
+  try {
+    form.parse(request, async (error, fields, files) => {
+      if (error) {
+        return response
+          .status(500)
+          .json({ msg: "Network Error: Failed to signup user" });
+      }
 
-        if (!username || !email || !password || !verifiedPassword) {
-          return response.status(500).json({ msg: "All fields are required" });
-        }
+      const { username, email, password, verifiedPassword } = fields;
 
-        if (password.trim().length < this.passwordLength) {
-          return response
-            .status(400)
-            .json({ msg: "Password has to be at least 8 characters" });
-        }
+      if (!username || !email || !password || !verifiedPassword) {
+        return response.status(500).json({ msg: "All fields are required" });
+      }
 
-        if (password.trim() !== verifiedPassword.trim()) {
-          return response.status(400).json({ msg: "Password's do not match" });
-        }
+      if (password.trim().length < Auth.passwordLength) {
+        return response
+          .status(400)
+          .json({ msg: "Password has to be at least 8 characters" });
+      }
 
-        const isUserAlreadyExising = await this.userModel.findOne({
-          email: email,
-        });
+      if (password.trim() !== verifiedPassword.trim()) {
+        return response.status(400).json({ msg: "Password's do not match" });
+      }
 
-        if (isUserAlreadyExising) {
-          return response
-            .status(400)
-            .json({ msg: "Account with this email already exist" });
-        }
+      const isUserAlreadyExising = await this.userModel.findOne({
+        email: email,
+      });
 
-        const salt = await Bcrypt.genSalt(this.hashRounds);
-        const hashedPassword = await Bcrypt.hash(password, salt);
-        const newUser = new this.userModel({
-          email: email,
-          username: username,
-          password: hashedPassword,
-        });
+      if (isUserAlreadyExising) {
+        return response
+          .status(400)
+          .json({ msg: "Account with this email already exist" });
+      }
 
-        const savedUser = await newUser.save();
+      const salt = await Bcrypt.genSalt(Auth.hashRounds);
+      const hashedPassword = await Bcrypt.hash(password, salt);
+      const newUser = new this.userModel({
+        email: email,
+        username: username,
+        password: hashedPassword,
+      });
 
-        const transporter = nodemailer.createTransport({
-          service: "SendinBlue",
-          auth: {
-            user: this.mailTransporterUser,
-            pass: this.mailTransporterPass,
-          },
-        });
+      const savedUser = await newUser.save();
 
-        const mailOptions = {
-          from: this.mailTransporterUser,
-          to: email,
-          subject: "Account Created",
-          html: `
+      const transporter = nodemailer.createTransport({
+        service: "SendinBlue",
+        auth: {
+          user: Auth.mailTransporterUser,
+          pass: Auth.mailTransporterPass,
+        },
+      });
+
+      const mailOptions = {
+        from: Auth.mailTransporterUser,
+        to: email,
+        subject: "Account Created",
+        html: `
 
                 <h1>Account Creation Successfull</h1>
 
             
             `,
-        };
+      };
 
-        transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-            return response.status(500).json({
-              msg: `Network Error: Failed to send confirmation email. ${
-                savedUser._id.length > 0
-                  ? "But Account was successfully created"
-                  : "Failed to create account"
-              }`,
-            });
-          }
-          return response
-            .status(200)
-            .json({ msg: "Account Successfully created" });
-        });
-      });
-    } catch (error) {
-      return response
-        .status(500)
-        .json({ msg: "Network Error: Failed to signup user" });
-    }
-  }
-
-  Login(request, response) {
-    const form = new Formidable.IncomingForm();
-
-    try {
-      form.parse(request, async (error, fields, files) => {
+      transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
-          return response
-            .status(500)
-            .json({ msg: "Network Error: Failed to signin user" });
+          return response.status(500).json({
+            msg: `Network Error: Failed to send confirmation email. ${
+              savedUser._id.length > 0
+                ? "But Account was successfully created"
+                : "Failed to create account"
+            }`,
+          });
         }
-
-        const { email, password } = fields;
-
-        if (!email || !password) {
-          return response.status(400).json({ msg: "All fields are required" });
-        }
-
-        const user = await this.userModel.findOne({ email: email });
-
-        if (!user) {
-          return response
-            .status(404)
-            .json({ msg: "Account with this email does not exist" });
-        }
-
-        const hashedPassword = user.password;
-        const isPasswordValid = await Bcrypt.compare(password, hashedPassword);
-
-        if (!isPasswordValid) {
-          return response.status(400).json({ msg: "Invalid Credentials" });
-        }
-
-        const isUserSessionExisting = await this.userSessions.findOne({
-          "session.user.id": user._id,
-        });
-        if (isUserSessionExisting) {
-          return response
-            .status(200)
-            .json({ msg: "Account already logged in" });
-        }
-
-        const userSessionObj = {
-          id: user._id,
-          email: user.email,
-        };
-
-        request.session.user = userSessionObj;
-
-        return response.status(200).send(request.sessionID);
+        return response
+          .status(200)
+          .json({ msg: "Account Successfully created" });
       });
-    } catch (error) {
-      return response
-        .status(500)
-        .json({ msg: "Network ErrorL Failed to sigin user" });
-    }
+    });
+  } catch (error) {
+    return response
+      .status(500)
+      .json({ msg: "Network Error: Failed to signup user" });
   }
-}
+};
 
-export default ExpressSessionAuth;
+Auth.Login = (request, response) => {
+  const form = new Formidable.IncomingForm();
+
+  try {
+    form.parse(request, async (error, fields, files) => {
+      if (error) {
+        return response
+          .status(500)
+          .json({ msg: "Network Error: Failed to signin user" });
+      }
+
+      const { email, password } = fields;
+
+      if (!email || !password) {
+        return response.status(400).json({ msg: "All fields are required" });
+      }
+
+      const user = await Auth.userModel.findOne({ email: email });
+
+      if (!user) {
+        return response
+          .status(404)
+          .json({ msg: "Account with this email does not exist" });
+      }
+
+      const hashedPassword = user.password;
+      const isPasswordValid = await Bcrypt.compare(password, hashedPassword);
+
+      if (!isPasswordValid) {
+        return response.status(400).json({ msg: "Invalid Credentials" });
+      }
+
+      const isUserSessionExisting = await Auth.userSessions.findOne({
+        "session.user.id": user._id,
+      });
+      if (isUserSessionExisting) {
+        return response.status(200).json({ msg: "Account already logged in" });
+      }
+
+      const userSessionObj = {
+        id: user._id,
+        email: user.email,
+      };
+
+      request.session.user = userSessionObj;
+
+      return response.status(200).send(request.sessionID);
+    });
+  } catch (error) {
+    return response
+      .status(500)
+      .json({ msg: "Network ErrorL Failed to sigin user" });
+  }
+};
+
+export default Auth;
