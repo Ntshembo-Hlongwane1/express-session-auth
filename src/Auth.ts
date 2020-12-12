3298d797a7f208477bfd9da018f1c19649bd1178
Ntshembo <juniorWebProjects@gmail.com>
2020-12-12 12:20:28 +0200

    Add: Docs

--- src/Auth.ts
+++ src/Auth.ts
@@ -15,6 +15,12 @@ interface Auth {
   passwordLength: number;
 }
 
+/**
+   
+  * @param 
+    
+ **/
+
 class AuthController implements Auth {
   userModel: Model<Document>;
   userSession: Model<Document>;
@@ -38,6 +44,14 @@ class AuthController implements Auth {
     this.mailTransporterPass = mailTransporterPass;
     this.passwordLength = passwordLength || 8;
   }
+
+  /**
+    
+    * @param request - First arguement to be passed is the express request
+    * @param response - Second arguement to be passed is the express response
+    * @returns - SignUp will return back a response to client with either 400 (When user has failed validations) | 500 (If server or network is down) | 201 (When the account has successfully been created)
+   
+  **/
 
   SignUp(request: Request, response: Response) {
     const form: Formidable.IncomingForm = new Formidable.IncomingForm();
@@ -95,6 +109,17 @@ class AuthController implements Auth {
         .json({ msg: "Network Error: Failed to signup user" });
     }
   }
+
+  /**
+    
+    * @param request - First arguement to be passed is the express request 
+    * @param response - Second arguement to be passed is the express response 
+    * @returns - Login method will return back an object and that you are to use to have your request.session.user
+    * @see - Check documenation on handling Login to work with express-session for cookie creation{@link https://github.com/Ntshembo-Hlongwane1/express-session-auth/blob/master/README.md#login}
+
+  
+  **/
+
   Login(request: Request, response: Response) {
     const form: Formidable.IncomingForm = new Formidable.IncomingForm();
 
